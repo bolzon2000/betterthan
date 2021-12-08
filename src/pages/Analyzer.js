4d277f00f809f7ce572fd57b2d40f154f03f9762
Bolton <davebolt@a07817b2aa07.ant.amazon.com>
2021-12-07 22:58:09 -0800

    image generates

--- src/pages/Analyzer.js
+++ src/pages/Analyzer.js
@@ -19,31 +19,49 @@ const Analyzer = () => {
    var [imageDescription, setDescription] = useState('...loading');
    var [imageURL, setURL] = useState("https://eu-west-2.admin.amplifyapp.com/static/media/amplify-logo.512cb92e.svg");
    const [externalData, setExternalData] = useState([]);
+   
    useEffect(() => {
         const getData = async () => {
             const models = await DataStore.query(Comparisons);
             setExternalData (models);
         }
         getData()
-    }, []);
-   
+    }, [externalData]);
 
-    const handleClick = () => {
-
-        if (count + 1 > externalData.length) {
-           setComplete(true);
-           setVote(10);
-        } else {
-            setCount(count + 1);
+    //first run will set items as soon as the external Data length 
+    //changes over to have items. Length only changes once.
+    useEffect(() => {
+        if (externalData.length) {
             setDescription(externalData[count].description);
             setURL(externalData[count].itemURL);
         }
-            
+    }, [externalData.length]);
+
+    useEffect(() => {
+        if ((count +1  > externalData.length) && (externalData.length != 0)) {
+            setComplete(true);
+        } else if (externalData.length > 0) {
+            setDescription(externalData[count].description);
+            setURL(externalData[count].itemURL);
+        }
+
+    }, [count] );
+
+
+   // if we try to setcount here, the update does not happen quickly enough
+   // because the setCount is batched or async     
+    const handleClick = (i) => {
+        if (isComplete) {
+            return;
+        }
+        setCount(count + 1);
+        setVote(votes + i);
+        console.log(votes);
     };
 
     const profileOverrides = {
-        "View.Image[0]": {src:thumbsUp,  onClick: handleClick},
-        "View.Image[1]": {src:thumbsDown, onClick: handleClick},
+        "View.Image[0]": {src:thumbsUp,  onClick: () => handleClick(1)},
+        "View.Image[1]": {src:thumbsDown, onClick: () => handleClick(0)},
         "View.Text[1]": {children:imageDescription},
         "View.Image[2]": {src: imageURL},
         "View": {height: "360px"}
@@ -53,7 +71,7 @@ const Analyzer = () => {
         return (
             <div className={styles.mainLayout}>
                 <Comparinator overrides={profileOverrides} />
-                <Link to="/Result" state={{votes: 10}}>
+                <Link to="/Result" state={{votes: votes}}>
                 <NSButton overrides={{"View.Text[0]": {children:"See the Results!"} }}/>
                 </Link>
             </div>
--- src/pages/Result.js
+++ src/pages/Result.js
@@ -3,18 +3,33 @@ import styles from '../css/mainLayoutGrid.module.css';
 import { NSButton } from "../ui-components"
 import { Link, useLocation } from "react-router-dom";
 import gauge0 from '../images/gauge0.png';
-
+import React, { useState, useEffect } from 'react';
 
 const Result = () => {
     const location = useLocation()
     const gaugeSrc = 'gauge' + (Math.trunc(location.state.votes) + Math.trunc(location.state.votes) % 2)*10;
     console.log(gaugeSrc); 
+    console.log("votes " + location.state.votes);
+
+   const [imageSrc, setImage] = useState(gauge0.png);
+    useEffect(() => {
+        const getData = async () => {
+            const response = await import("../images/" + gaugeSrc + ".png");
+            setImage(response.default);
+        }
+        getData()
+    }, [imageSrc]);
+
+   
+
+     // change relative path to suit your needs
+                
 
     return (
     <div className = {styles.mainLayout}>
-        <GaugeInator overrides={{"View.Image[0]":{src: gauge0}}} />
+        <GaugeInator overrides={{"View.Image[0]":{src: imageSrc}}} />
         <p style={{padding:"20px"}}>Thanks for playing. Hopefully this application has given you some insight
-            into how much you appreciate Nikhil. Not having a score in the green area of the 
+            into how much you appreciate Nikhil. Not having a score in the red area of the 
             gauge may indicate a lack of appreciation for your awesome PM.
         </p>
 
